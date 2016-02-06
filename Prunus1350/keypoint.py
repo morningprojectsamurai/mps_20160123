e5694bf391ea2cb3f0294f4f322882f213460933
Prunus1350 <Prunus1350@gmail.com>
2016-02-07 03:08:56 +0900

    create extract_keypoints function

--- Prunus1350/keypoint.py
+++ Prunus1350/keypoint.py
@@ -8,8 +8,46 @@ from dog import dog
 def extract_keypoints(octave, threshold):
     ## ここに処理を書く
     keypoints = []
+    for i in range(1, octave.shape[0]-1):
+        for x in range(1, octave[i].shape[0]-1):
+            for y in range(1, octave[i].shape[1]-1):
+                v = [
+                    octave[i-1][x-1][y-1],                
+                    octave[i-1][x-1][y],                
+                    octave[i-1][x-1][y+1],                
+                    octave[i-1][x][y-1],                
+                    octave[i-1][x][y],                
+                    octave[i-1][x][y+1],                
+                    octave[i-1][x+1][y-1],                
+                    octave[i-1][x+1][y],                
+                    octave[i-1][x+1][y+1],                
+                    octave[i][x-1][y-1],                
+                    octave[i][x-1][y],                
+                    octave[i][x-1][y+1],                
+                    octave[i][x][y-1],                
+                    octave[i][x][y],                
+                    octave[i][x][y+1],                
+                    octave[i][x+1][y-1],                
+                    octave[i][x+1][y],                
+                    octave[i][x+1][y+1],                
+                    octave[i+1][x-1][y-1],                
+                    octave[i+1][x-1][y],                
+                    octave[i+1][x-1][y+1],                
+                    octave[i+1][x][y-1],                
+                    octave[i+1][x][y],                
+                    octave[i+1][x][y+1],                
+                    octave[i+1][x+1][y-1],                
+                    octave[i+1][x+1][y],                
+                    octave[i+1][x+1][y+1]                
+                ]
+                check_v = octave[i][x][y]
+                max_v = max(v)
+                min_v = min(v)
+                diff_v = max_v - min_v
+                if check_v in (max_v, min_v) and diff_v >= threshold:
+                    keypoints.append((x, y, diff_v))
 
-    return keypoints
+    return np.array(keypoints)
 
 
 if __name__ == '__main__':
@@ -26,16 +64,15 @@ if __name__ == '__main__':
     keypoint_space = []
     for octave in dog_space:
         keypoint_space.append([])
-        keypoint_space[-1].append(extract_keypoints(octave, 0))
+        keypoint_space[-1].append(extract_keypoints(octave, 0.005))
 
     print('Draw keypoints')
     plt.imshow(lena_img, cmap='Greys_r')
     fig = plt.gcf()
     for n, octave in enumerate(keypoint_space):
-        r = 1
         for l, layer in enumerate(octave):
             print(len(layer))
             for p in layer:
-                p *= np.power(2, n)
-                fig.gca().add_artist(plt.Circle((p[1], p[0]), r, color='r', fill=False))
+                p[:2] *= np.power(2, n)
+                fig.gca().add_artist(plt.Circle((p[1], p[0]), p[2]*300, color='r', fill=False))
     plt.show()
